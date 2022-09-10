a0a5a850f0a0699939f6e0784784ea7b18ddb71d
Tomo-kw <mulberryfield412@yahoo.ne.jp>
2022-09-10 19:59:17 +0900

    現在の色をローカルストーレジに保存・削除処理を実装

--- src/components/ChangeButton.jsx
+++ src/components/ChangeButton.jsx
@@ -2,17 +2,35 @@ import { Button } from '@mui/material'
 import { useState } from 'react'
 
 const ChangeButton = () => {
-    const [color, setColor] = useState('inherit')
-
+    const IsColor = localStorage.getItem("IsColor")
+    const changeIsColor = IsColor === 'true' ? true : false
+    const [isBgColor, setIsBgColor] = useState(changeIsColor)
+    const bgColor = isBgColor ? 'success' : 'inherit'
     const onclick = () => {
-        console.log('ボタンクリックされた')
-        setColor('success')
+        setIsBgColor(!isBgColor)
+    }
+
+    // ローカルストレージに保存
+    const saveLocalStorage = () => {
+        console.log('save')
+        localStorage.setItem("IsColor", isBgColor)
+    }
+
+
+    // ローカルストレージから削除
+    const deleteLocalStorage = () => {
+        console.log('delete')
+        localStorage.removeItem("IsColor")
     }
 
     return (
         <div>
-            <p>押したらボタンの背景色が変わる</p>
-            <Button variant="contained" color={ color } onClick={ onclick }>ボタン</Button>
+            <p>ボタンを押したらボタンの背景色が変わる</p>
+            <Button variant="contained" color={ bgColor } onClick={ onclick }>ボタン</Button>
+            <br />
+            <br />
+            <Button variant="contained" color= 'primary' onClick={ saveLocalStorage }>保存</Button>
+            <Button variant="contained" color= 'error' onClick={ deleteLocalStorage }>削除</Button>
         </div>
     );
 } 
